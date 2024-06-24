fdc3c00a45f81f1a8e6a1606544f7c7c00ca125e
Rylan Schubkegel <rylanschubkegel@gmail.com>
2024-06-24 10:30:39 -0700

    Add custom domain to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  // https://docs.astro.build/en/guides/deploy/github/#using-github-pages-with-a-custom-domain
+  site: 'https://hcc.rschubkegel.xyz',
+})
